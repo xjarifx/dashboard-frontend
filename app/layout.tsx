89f34aa6779a484559a98fbf98ea063ae91fc0c4
jarif <xjarifx@gmail.com>
2026-03-11 11:37:37 +0600

    feat: add metadata for improved SEO and page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,14 @@ import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Metadata } from "next";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Meridian",
+  description: "Product Analytics",
+};
 
 export default function RootLayout({
   children,
