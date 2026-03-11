42a3f8e9685c410478003a077bd76cace739a825
jarif <xjarifx@gmail.com>
2026-03-11 11:30:14 +0600

    feat: simplify Home component by redirecting to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Link href="/dashboard">
-        <button className="mt-4 px-4 py-2 bg-zinc-800 text-white rounded hover:bg-zinc-700">
-          Go to Dashboard
-        </button>
-      </Link>
-    </div>
-  );
+  redirect("/dashboard");
 }
